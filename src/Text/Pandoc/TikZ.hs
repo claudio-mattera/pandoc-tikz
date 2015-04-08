f1cbd8b156a24444ec1029ae3c574305da95b6c5
Claudio Mattera <claudio@mattera.it>
2015-04-08 22:34:29 +0200

    Implemented replacement of Latex source with images.

--- src/Text/Pandoc/TikZ.hs
+++ src/Text/Pandoc/TikZ.hs
@@ -4,6 +4,10 @@ module Text.Pandoc.TikZ where
 import Text.Pandoc
 import Text.Pandoc.Walk (query, walk)
 
+import Data.ByteString.Char8 (pack, unpack)
+import Data.ByteString.Base16 (encode)
+import Crypto.Hash.SHA256 (hash)
+
 newtype LatexSource = LatexSource String
                       deriving (Eq, Show)
 
@@ -15,10 +19,15 @@ extractLatexSources :: Pandoc -> [LatexSource]
 extractLatexSources = query extractLatexSource
 
 replaceLatexSourceWithHashImage :: Block -> Block
-replaceLatexSourceWithHashImage = undefined
+replaceLatexSourceWithHashImage (RawBlock (Format "latex") source) =
+  Para [Image [] (hashString source ++ ".png", "fig:")]
+replaceLatexSourceWithHashImage x = x
 
 replaceLatexSourceWithHashImages :: Pandoc -> Pandoc
 replaceLatexSourceWithHashImages = walk replaceLatexSourceWithHashImage
 
 readDoc :: String -> Pandoc
 readDoc = readMarkdown def
+
+hashString :: String -> String
+hashString = unpack . encode . hash . pack
