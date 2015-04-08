779b32a3105f4d04ccbb9be6f023350d7906d970
Claudio Mattera <claudio@mattera.it>
2015-04-09 00:24:55 +0200

    Added application stub.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,14 @@
 
 module Main where
 
+import System.Environment (getArgs)
+
+import Text.Pandoc.TikZ (processDocument, readDoc, writeDoc)
+
 main :: IO ()
-main = return ()
+main = do
+  (inputFileName:outputFileName:_) <- getArgs
+  inputFile <- readFile inputFileName
+  let inputDocument = readDoc inputFile
+  outputDocument <- processDocument inputDocument
+  writeFile outputFileName . writeDoc $ outputDocument
