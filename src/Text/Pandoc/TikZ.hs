9e133a7d3b94fbc81df1b68e106a8ca0c38215e5
Claudio Mattera <claudio@mattera.it>
2015-04-08 21:35:39 +0200

    Initial commit.

---
+++ src/Text/Pandoc/TikZ.hs
@@ -0,0 +1,2 @@
+
+module Text.Pandoc.TikZ where
---
+++ test/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main :: IO ()
+main =
+  defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Unit tests"
+  [
+  ]
