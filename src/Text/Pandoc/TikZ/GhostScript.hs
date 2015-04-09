8b77338a734fbeb0105620bcee72062a62ad20cf
Claudio Mattera <claudio@mattera.it>
2015-04-09 21:42:09 +0200

    Removing PDF files after converting them to PNG.

--- src/Text/Pandoc/TikZ/GhostScript.hs
+++ src/Text/Pandoc/TikZ/GhostScript.hs
@@ -10,6 +10,11 @@ import System.Process (proc,
 import System.IO (hGetContents)
 import System.Exit (ExitCode(ExitSuccess, ExitFailure))
 
+import Prelude hiding (catch)
+import System.Directory (removeFile)
+import Control.Exception (catch, throwIO)
+import System.IO.Error (isDoesNotExistError)
+
 convertPDFtoPNG :: FilePath -> FilePath -> FilePath -> IO (Either String ())
 convertPDFtoPNG ghostScriptPath pdfFileName pngFileName = do
   let arguments = [ "-sDEVICE=pngalpha"
@@ -22,8 +27,15 @@ convertPDFtoPNG ghostScriptPath pdfFileName pngFileName = do
   (Nothing, Just _, Just stderrHandle, handle) <-
     createProcess process { std_out = CreatePipe, std_err = CreatePipe }
   exitCode <- waitForProcess handle
+  removeIfExists pdfFileName
   case exitCode of
     ExitFailure v -> do
       msg <- hGetContents stderrHandle
       error $ "Exit code: " ++ show v ++ ", output: " ++ msg
     ExitSuccess -> return $ Right ()
+
+removeIfExists :: FilePath -> IO ()
+removeIfExists fileName = removeFile fileName `catch` handleExists
+  where handleExists e
+          | isDoesNotExistError e = return ()
+          | otherwise = throwIO e
